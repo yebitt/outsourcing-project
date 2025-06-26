c8beb1778063ee9de55d4fe0835cd473e58d4ad0
yebitt <yebin0519@naver.com>
2025-06-26 10:43:52 +0900

    initial commit

---
+++ src/main/java/com/example/outsourcingproject/OutsourcingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.outsourcingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsourcingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutsourcingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/outsourcingproject/OutsourcingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.outsourcingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutsourcingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
